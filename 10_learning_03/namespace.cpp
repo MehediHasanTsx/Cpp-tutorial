ac0c9df4b1c19bcec76e39b311b0f0bfe7a42c04
Mehedi <mehedi.hasan.tsx@gmail.com>
2025-07-09 21:19:40 +0600

    const keyword

---
+++ 10_learning_03/namespace.cpp
@@ -0,0 +1,8 @@
+// Namespace = provides a solution for preventing name conflicts in large projects. Each identically named entities as long as the namespaes are different.
+
+#include <iostream>
+
+int main()
+{
+    int x = 0;
+}
--- 9_learning_02/const_keyword.cpp
+++ 9_learning_02/const_keyword.cpp
@@ -6,14 +6,21 @@ int main()
     // tells the compiler to prevent anything from modifying it
     // (read-only)
 
-    double pi = 3.14159;
+    const double PI = 3.14159; // now this const cant be changed in anywhere of this progarm
     double radius = 10;
-    double circumference = 2 * pi * radius;
-    double area = pi * radius * radius;
+    double circumference = 2 * PI * radius;
+    double area = PI * radius * radius;
 
     std::cout << "The circumference of the circle is " << circumference << "\n";
 
     std::cout << "The area of the circle is " << area;
 
+    // More const value like below
+
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGT = 1080;
+
+    
     return 0;
 }
