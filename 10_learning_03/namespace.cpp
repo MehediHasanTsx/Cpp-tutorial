ce6c9f62ccf3fca51be1222f4de030a2e461d9a5
Mehedi <mehedi.hasan.tsx@gmail.com>
2025-07-09 21:37:48 +0600

    using namespace std

--- 10_learning_03/namespace.cpp
+++ 10_learning_03/namespace.cpp
@@ -2,7 +2,30 @@
 
 #include <iostream>
 
+namespace first
+{
+    int x = 1;
+}
+
+namespace second
+{
+    int x = 2;
+}
+
 int main()
 {
-    int x = 0;
+
+    // to output second namespace without scope resulation operator
+    using namespace second;
+    std::cout << x << "\n";
+
+    int x = 6;
+    std::cout << x << "\n";
+
+    // to output first namespace with scope resulation operator (::)
+    std::cout << first::x << "\n";
+
+    // when i use namespace std; in after #include<iostream> then the output will be like = cout << "Hello world !" << endl; can also write like below
+
+    // when i don't use that then the output will be like = std::cout << "Hello world !" << "\n";
 }
