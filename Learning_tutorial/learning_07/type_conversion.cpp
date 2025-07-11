6d75845d29498a379ff0aec1f0d86b0ae333a763
Mehedi <mehedi.hasan.tsx@gmail.com>
2025-07-11 20:07:58 +0600

    type_conversion

--- Learning_tutorial/learning_07/type_conversion.cpp
+++ Learning_tutorial/learning_07/type_conversion.cpp
@@ -3,8 +3,22 @@
 int main()
 {
     // type conversion = conversion a  value of one data type to another
-    // Implicit = automatic
-    // Explicit = Precede value with new data type (int )
+    //                   Implicit = automatic
+    //                   Explicit = Precede value with new data type (int)
+
+    double x = (int)3.33;
+    std::cout << x << '\n';
+
+    char x1 = 100;
+    std::cout << x1 << '\n'; // implicit
+
+    std::cout << (char)97 << '\n'; // explicit
+
+    // example
+    int correct = 33;
+    int questions = 100; // can add double here but also like below
+    double score = correct / (double)questions * 100;
+    std::cout << score << "%" << '\n';
 
     return 0;
 }
