ccfdd7e3287d835db6d3b3ea7679a5405c3cf35a
Mehedi <mehedi.hasan.tsx@gmail.com>
2025-07-16 19:07:22 +0600

    if statement

--- Learning_tutorial/learning_11_if_statements/if_statements.cpp
+++ Learning_tutorial/learning_11_if_statements/if_statements.cpp
@@ -1,8 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    
+    // if statements = do something if a condition is true.
+    //          if not, then don't do it.
+
+    cout << "Enter your age : ";
+    int age;
+    cin >> age;
+
+    if (age >= 80)
+    {
+        cout << "You are too old." << '\n';
+    }
+    else if (age >= 18)
+    {
+        cout << "You are welcome." << '\n';
+    }
+    else if (age < 0)
+    {
+        cout << "You haven't been born yet!" << '\n';
+    }
+    else
+    {
+        cout << "You are not welcome." << '\n';
+    }
+
     return 0;
 }
