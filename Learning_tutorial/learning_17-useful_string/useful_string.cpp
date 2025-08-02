8eb3a0fa22916420f14d10eff793be012b4c744f
Mehedi <mehedi.hasan.tsx@gmail.com>
2025-08-03 00:40:27 +0600

    useful string

--- Learning_tutorial/learning_17-useful_string/useful_string.cpp
+++ Learning_tutorial/learning_17-useful_string/useful_string.cpp
@@ -7,16 +7,37 @@ int main()
 
     cout << "Enter your name : ";
     // cin >> name; //this wont count space between inputs
-    getline(cin, name); // this will count space between inputs 
+    getline(cin, name); // this will count space between inputs
 
-    if (name.length() > 12)
+    if (name.length() > 12) // length method
     {
-        cout << "Your name can't be over 12 character." << '\n';
+        cout << "Your name can't be over 12 character.";
+    }
+    else if (name.empty()) // empty method
+    {
+        cout << "You didn't enter your name.";
     }
     else
     {
-        cout << "Welcome " << name << '\n';
+        cout << "Hello " << name;
     }
+
+    /*name.clear(); // it will clear the input name
+    {
+        cout << "Hello " << name;
+    }*/
+
+    /*name.append("@gamil.com");  // output will be name@gmail.com
+     cout << "Your username is now " << name ;*/
+    // cout << name.append("@gmail.com"); // can be write like this
+
+    // cout << name.at(9); // show the nth caracter string form the input
+
+    // cout << name.insert(0, "@"); // exact position insert any string
+
+    // cout << name.find(' '); // it will find the defined character or string
+
+    // cout << name.erase(0, 5); // it will erase defined strings
 
     return 0;
 }
